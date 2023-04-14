2124f14b5b9acc1febcdb07846f8e6a0c4aed8a0
heycn <heycn@foxmail.com>
2023-04-14 12:25:27 +0800

    feat: 支持左右滑动与透明度

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,6 +1,6 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Link, useLocation, useNavigate, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 import { useSwipe } from '../hooks/useSwipe'
@@ -10,6 +10,11 @@ const linkMap: Record<string, string> = {
   '/welcome/3': '/welcome/4',
   '/welcome/4': '/welcome/xxx',
 }
+const backwardLinkMap: Record<string, string> = {
+  '/welcome/2': '/welcome/1',
+  '/welcome/3': '/welcome/2',
+  '/welcome/4': '/welcome/3'
+}
 export const WelcomeLayout: React.FC = () => {
   const animating = useRef(false)
   const map = useRef<Record<string, ReactNode>>({})
@@ -17,29 +22,38 @@ export const WelcomeLayout: React.FC = () => {
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
   const [extraStyle, setExtraStyle] = useState<{ position: 'relative' | 'absolute' }>({ position: 'relative' })
-  const transitions = useTransition(location.pathname, {
-    from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
-    enter: { transform: 'translateX(0%)' },
-    leave: { transform: 'translateX(-100%)' },
-    config: { duration: 300 },
-    onStart: () => {
-      setExtraStyle({ position: 'absolute' })
-    },
-    onRest: () => {
-      animating.current = false
-      setExtraStyle({ position: 'relative' })
-    }
-  })
   const main = useRef<HTMLElement>(null)
   const { direction } = useSwipe(main, { onTouchStart: e => e.preventDefault() })
+  const transitionConfig = useMemo(() => {
+    const translateX = direction === 'right' ? -100 : 100
+    const first = location.pathname === '/welcome/1' && direction === ''
+    return {
+      from: { opacity: first ? 1 : 0, transform: `translateX(${first ? 0 : translateX}%)` },
+      enter: { opacity: 1, transform: 'translateX(0%)' },
+      leave: { opacity: 0, transform: `translateX(${-translateX}%)`, },
+      config: { duration: 350 },
+      onStart: () => setExtraStyle({ position: 'absolute' }),
+      onRest: () => {
+        animating.current = false
+        setExtraStyle({ position: 'relative' })
+      }
+    }
+  }, [direction, location.pathname])
+  const transitions = useTransition(location.pathname, { ...transitionConfig })
   const nav = useNavigate()
   useEffect(() => {
     if (direction === 'left') {
       if (animating.current) { return }
       animating.current = true
-      nav(linkMap[location.pathname])
+      nav(linkMap[location.pathname], { replace: true })
     }
-  }, [direction, location.pathname, linkMap])
+    if (direction === 'right') {
+      if (animating.current) { return }
+      if (location.pathname === '/welcome/1') { return }
+      animating.current = true
+      nav(backwardLinkMap[location.pathname], { replace: true })
+    }
+  }, [direction])
   return (
     <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px>
       <header shrink-0 text-center pt-64px>
